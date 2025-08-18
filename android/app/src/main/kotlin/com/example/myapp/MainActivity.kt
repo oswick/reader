096b640778a4cddc02dd9a8cfdb4ba742c3e6bc3
oswaldo.reggae <oswaldo.reggae@gmail.com>
2025-08-18 14:50:08 +0000

    hola?

--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,5 +1,37 @@
-package com.example.myapp
+package com.example.your_project_name
 
+import android.content.pm.PackageManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.Manifest
+import android.os.Build
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val REQUEST_CODE = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permiso concedido
+                } else {
+                    // Permiso denegado
+                }
+                return
+            }
+        }
+    }
+}
